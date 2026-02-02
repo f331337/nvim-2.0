afa593c4f5b904dd5534ab90916f0f36dd1fe7ee
Nico <feeleet@proton.me>
2026-02-02 11:49:47 +0200

    added orgmode

---
+++ lua/nico/plugins/orgmode.lua
@@ -0,0 +1,24 @@
+return {
+  'nvim-orgmode/orgmode',
+  event = 'VeryLazy',
+  ft = { 'org' },
+  config = function()
+    -- Setup orgmode
+    require('orgmode').setup({
+      org_agenda_files = '~/orgfiles/**/*',
+      org_default_notes_file = '~/orgfiles/refile.org',
+      mappings = {
+        org = {
+          org_toggle_checkbox = "<leader>tc" }
+      }
+    })
+
+    -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
+    -- add ~org~ to ignore_install
+    -- require('nvim-treesitter.configs').setup({
+    --   ensure_installed = 'all',
+    --   ignore_install = { 'org' },
+    -- })
+  end,
+}
+
--- required_utils.sh
+++ required_utils.sh
@@ -5,3 +5,7 @@ uvx install ty
 npm install -g typescript-language-server typescript
 
 elixir elixirls  tree-sitter-cli
+
+
+go  install -v golang.org/x/tools/gopls@latest
++ add path of gobin tools
