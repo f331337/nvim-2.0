1809f738812554bcc71655c3a862f737741628b3
Nico <feeleet@proton.me>
2026-03-24 17:58:21 +0200

    [feat]: update telescope

--- lua/nico/plugins/telescope.lua
+++ lua/nico/plugins/telescope.lua
@@ -1,18 +1,23 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  keys = {
+    'nvim-telescope/telescope.nvim', version = '0.2.1',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        -- optional but recommended
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    },
+      keys = {
     -- disable the keymap to grep files
     { "<leader>/",  false },
     -- change a keymap
     { "<leader>tf", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
     { "<leader>tg", "<cmd>Telescope live_grep<cr>", desc = "Find Word" },
-    { "<leader>tg", "<cmd>Telescope live_grep<cr>", desc = "Find Word" },
     { "<leader>ts", "<cmd>Telescope grep_string<cr>", desc = "Find word (buffer)" },
     {
       "<leader>tp",
-      function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
+      function() 
+        require("telescope.builtin").find_files({ 
+          cwd = require("lazy.core.config").options.root 
+        }) 
+      end,
       desc = "Find Plugin File",
-    },
-  },
-}
-
+}}}
